383dc07b07f9ec1d22b0ae657a273d1b09661824
wstygg <wstygg@student.21-school.ru>
2020-04-21 17:29:43 +0800

    deliting stuff

--- Sources/main.c
+++ Sources/main.c
@@ -6,7 +6,7 @@
 /*   By: wstygg <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/07 15:30:33 by wstygg            #+#    #+#             */
-/*   Updated: 2020/04/21 17:26:52 by wstygg           ###   ########.fr       */
+/*   Updated: 2020/04/21 17:27:12 by wstygg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int						main(void)
 {
-	const char			*path = "../Conf.conf";
+	const char			*path = "Conf.conf";
 	t_list				*task_list;
 
 	conf_help();
 	task_list = conf_read(path);
 	print_tasks(task_list);
-	task_list = conf_reload("../Conf_2.conf", task_list);
+	task_list = conf_reload("Conf_2.conf", task_list);
 	print_tasks(task_list);
 	task_list_delete(task_list);
 	exit(0);
